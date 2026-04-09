baae0a5f4b039675865cdfd6748442f1864a0721
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-04-09 17:26:39 +0530

    tst

---
+++ grater/internal/dependent/dep_test.go
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package dependent represents a dependent module in the .grater directory.
+
+//go:build integration
+// +build integration
+
+package dependent
